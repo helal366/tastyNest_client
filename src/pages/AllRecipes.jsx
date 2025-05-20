ed419f715f6a5fa8fe6905fa9d66f1807d16dac4
Helal366 <helal56876@gmail.com>
2025-05-20 19:24:19 +0600

    loader created

--- src/pages/AllRecipes.jsx
+++ src/pages/AllRecipes.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
+import RecipeCard from '../components/RecipeCard';
 
 const AllRecipes = () => {
     const allRecipes=useLoaderData();
     console.log(allRecipes);
     return (
         <div>
-            <h2 className='text-center text-2xl font-semibold py-4'>All recipes: {allRecipes.length}</h2>
+            <h2 className='text-center text-2xl font-semibold py-4'>All recipes: {allRecipes?.length || 0}</h2>
             <div>
                 {
-                    allRecipes.map((recipe, i)=>)
+                    allRecipes?.map((recipe, i)=><RecipeCard key={i} recipe={recipe}></RecipeCard>)
                 }
             </div>
         </div>
